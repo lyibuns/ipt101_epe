e6b9d175c5733e49dd4803e88f43656e50338053
lyibuns <beatricelynnre@gmail.com>
2023-03-28 12:00:53 +0800

    sample

---
+++ sample.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$num ="5";
+
+echo "Multiplication of $num<br><br>";
+
+$i = 1;
+for ($i; $i <= 10; $i++) {
+    echo "\t", $num, " X ", $i, " = ", $num * $i, "<br>";
+}
+?>
+
+</body>
+</html>
